4a9c6074f6312f0914ccbfb47ce9f7ccf233d90e
Francesco Ognibene <ogibenefra@gmail.com>
2024-05-01 13:27:03 +0200

    Fixed hbs (closes #79) Now the downloader uses a publication zip that contains a sqlite database with the json we need. This is a bit of a hack but for now it should work. Sadly the efficient downloader we once had is gone :-(

--- services/hbs.py
+++ services/hbs.py
@@ -2,6 +2,11 @@ import requests
 import fitz
 import lib
 import config
+from io import BytesIO
+import zipfile
+import tempfile
+import sqlite3
+import json
 
 service = "hbs"
 
@@ -20,15 +25,9 @@ def getlibrary(token, platform):
 	r = requests.get("https://ms-api.hubscuola.it/getLibrary/" +  platform, headers={"Token-Session": token})
 	return r.json()
 
-def getbookinfo(token, bookid, platform):
-	r = requests.get("https://ms-api.hubscuola.it/me" + platform + "/publication/" + str(bookid), headers={"Token-Session": token})
-	return r.json()
-
-def downloadchapter(token, bookid, chapterid, progress, total, done):
-	from io import BytesIO
-	import zipfile
-	
-	r = requests.get("https://ms-mms.hubscuola.it/public/" + str(bookid) + "/" + str(chapterid) + ".zip", params={"tokenId": token, "app": "v2"}, headers={"Token-Session": token}, auth=("testusername", "testpassword"), stream=True)
+def downloadzip(token, bookid, progress, total, done, chapterid="publication"):
+	suburl = "downloadPackage" if chapterid == "publication" else "public"
+	r = requests.get("https://ms-mms.hubscuola.it/" + "/".join([suburl, bookid, chapterid + ".zip"]), params={"tokenId": token, "app": "v2"}, headers={"Token-Session": token}, stream=True)
 	length = int(r.headers.get("content-length", 1))
 	file = b""
 	for data in r.iter_content(chunk_size=102400):
@@ -43,19 +42,6 @@ def cover(token, bookid, data):
 def checktoken(token):
 	r = requests.get("https://ms-api.hubscuola.it/annotation/user-preferences", headers={"Token-Session": token})
 	return not "not valid" in r.text
-
-def getauth(jwt, bookid, platform):
-	r = requests.post("https://ms-pdf.hubscuola.it/i/d/" + bookid + "/auth", json={"jwt": jwt, "origin": "https://" + platform + ".hubscuola.it/viewer/" + bookid + "?page=1"}, headers={"PSPDFKit-Platform": "web", "PSPDFKit-Version": "protocol=3, client=2020.6.0, client-git=63c8a36705"})
-	return r.json()
-
-def downloadpdf(token, bookid, layerhandle, progress, total, done):
-	r = requests.get("https://ms-pdf.hubscuola.it/i/d/" + bookid + "/h/" + layerhandle + "/pdf?token=" + token, stream=True)
-	length = int(r.headers.get("content-length", 1))
-	file = b""
-	for data in r.iter_content(chunk_size=102400):
-		file += data
-		progress(round(done + len(file) / length * total))
-	return file
 
 def extractpage(zipfile, chapterid, pageid):
 	file = zipfile.read(str(chapterid) + "/" + str(pageid) + ".pdf")
@@ -85,9 +71,13 @@ def library(token):
 
 	return books
 
-def downloadbook_legacy(token, bookid, data, progress):
+def downloadbook(token, bookid, data, progress):
 	progress(0, "Getting book info")
-	bookinfo = getbookinfo(token, bookid)
+	bookinfozip = downloadzip(token, bookid, progress, 5, 0)
+	with tempfile.NamedTemporaryFile() as tf:
+		tf.write(bookinfozip.read("publication/publication.db"))
+		cur = sqlite3.connect(tf.name).cursor()
+		bookinfo = json.loads(cur.execute("SELECT offline_value FROM 'offline_tbl' WHERE offline_path = 'meyoung/publication/" + bookid + "';").fetchone()[0])
 
 	pdf = fitz.Document()
 	toc = []
@@ -99,7 +89,7 @@ def downloadbook_legacy(token, bookid, data, progress):
 	for i, chapter in enumerate(chapters):
 		chapterstart = 5 + (i * chapterwidth)
 		progress(chapterstart, f"Downloading unit {i + 1}/{len(chapters)}")
-		chapterzip = downloadchapter(token, bookid, chapter["chapterId"], progress, chapterwidth, chapterstart)
+		chapterzip = downloadzip(token, bookid, progress, chapterwidth, chapterstart, str(chapter["chapterId"]))
 		toc.append([1, chapter["title"], pagecount])
 		for j in chapter["children"]:
 			if isinstance(j, int):
@@ -121,48 +111,4 @@ def downloadbook_legacy(token, bookid, data, progress):
 		labels = bookinfo["multiRangeIndex"]["valToLabel"].values()
 		pdf.set_page_labels(lib.generatelabelsrule(labels))
 	pdf.set_toc(toc)
-	return pdf
-
-def downloadbook_new(token, bookid, data, progress):
-	progress(0, "Getting book info")
-	bookinfo = getbookinfo(token, bookid, data["platform"])
-
-	progress(2, "Fetching authentication data")
-	auth = getauth(bookinfo["jwt"], bookid, data["platform"])
-
-	progress(4, "Downloading unencrypted pdf")
-	pdfbytes = downloadpdf(auth["token"], bookid, auth["layerHandle"], progress, 91, 4)
-
-	pdf = fitz.Document(stream=pdfbytes, filetype="pdf")
-
-	progress(98, "Applying toc/labels")
-	toc = []
-
-	pagecount = 1
-	chapters = bookinfo["indexContents"]["chapters"]
-
-	for i, chapter in enumerate(chapters):
-		toc.append([1, chapter["title"], pagecount])
-		for j in chapter["children"]:
-			if isinstance(j, int):
-				if j in bookinfo["pagesId"]:
-					pagecount += 1
-			else:
-				if len(chapter["children"]) > 1:
-					toc.append([2, j["title"], pagecount])
-				for k in j["children"]:
-					if k in bookinfo["pagesId"]:
-						pagecount += 1
-
-	if config.getboolean(service, "PageLabels", fallback=False):
-		labels = bookinfo["multiRangeIndex"]["valToLabel"].values()
-		pdf.set_page_labels(lib.generatelabelsrule(labels))
-	pdf.set_toc(toc)
-	return pdf
-
-def downloadbook(token, bookid, data, progress):
-	if config.getboolean(service, "UseLegecy", fallback=False):
-		pdf = downloadbook_legacy(token, bookid, data, progress)
-	else:
-		pdf = downloadbook_new(token, bookid, data, progress)
 	return pdf
